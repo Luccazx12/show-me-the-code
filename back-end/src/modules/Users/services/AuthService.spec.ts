4da79e67b3792910d060708e7924584939c74988
Lucca <lucca@pop-os.localdomain>
2022-02-10 11:14:32 -0300

    Teste de autenticação de usuário atualizado!

---
+++ back-end/src/modules/Users/services/AuthService.spec.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import { SigninService } from './AuthService';
+import { GetByUsernameService } from './GetByUsernameService';
+
+describe('AuthService', () => {
+  const authenticateUser = new SigninService();
+  const findUserByUsername = new GetByUsernameService();
+
+  it('should be able to authenticate user', async () => {
+    const user = await findUserByUsername.execute('admin');
+
+    // Admin Account (seeders)
+    //name: 'Admin',
+    //username: 'admin',
+    //password: password,
+    //email: 'mario-lucca@hotmail.com',
+    ////Estado de sp (uf)
+    //state: 35,
+    ////Cidade de sp (municipio)
+    //city: 3550308,
+    //activated: true,
+
+    const auth = await authenticateUser.execute(
+      'mario-lucca@hotmail.com',
+      '1234',
+    );
+
+    expect(auth).toHaveProperty('token');
+    expect(auth).toEqual(user);
+  });
+
+  it('should not be able to authenticate with wrong passowrd', async () => {
+    await expect(
+      authenticateUser.execute('mario-lucca@hotmail.com', 'wrong-password'),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
